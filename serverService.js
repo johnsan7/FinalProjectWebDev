1f55caa9c51fe87b1c6f83d5997215a826e6b2d8
johnsan7 <johnsan7@oregonstate.edu>
2015-12-03 19:25:00 -0600

    fred

--- serverService.js
+++ serverService.js
@@ -74,6 +74,7 @@ console.log("getting to tables function, should be sending back everything")
       return;
     }
 console.log("Got to after the mysql query");
+	res.setHeader('Content-Type', 'application/json');
     results = JSON.stringify(rows);
     res.send(results);
   });
