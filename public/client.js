e9d43282ce2eb63b79e38c9945f9bd220ccb037d
johnsan7 <johnsan7@oregonstate.edu>
2015-12-04 12:00:01 -0600

    fred

--- public/client.js
+++ public/client.js
@@ -12,7 +12,9 @@ console.log("Client Javascript is running");
 
 //app.use(express.static('public'));
 
-document.addEventListener('DOMContentLoaded', drawTable);
+//document.addEventListener('DOMContentLoaded', drawTable);
+
+drawTable();
 
 //First we will reset the database. When this loads, it will simply draw the table. 
 
