fa825d5bd25c7239144971a388d6c3bfde4f4640
johnsan7 <johnsan7@oregonstate.edu>
2015-12-04 10:33:46 -0600

    fred

--- public/client.js
+++ public/client.js
@@ -157,5 +157,7 @@ function deleteRow (button)
 	console.log("Delete ID passed by button was: ", button.nextSibling.name);
 	
 };
+
+console.log(document.getElementsByClassName(deleteButton).nextSibling.value);
 		
 		
