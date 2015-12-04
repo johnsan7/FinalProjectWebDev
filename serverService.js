9e573465d0f9987a54a0a26c3aa022a33635d2db
johnsan7 <johnsan7@oregonstate.edu>
2015-12-03 18:06:05 -0600

    i

--- serverService.js
+++ serverService.js
@@ -53,7 +53,7 @@ app.get('/reset-table',function(req,res,next){
     "lbs BOOLEAN)";
     mysql.pool.query(createString, function(err){
       context.results = "Table reset";
-      res.send(context.results);
+      res.send("Table Reset");
     })
   });
 });
