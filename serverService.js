ab7d58e8888404009c741a39cba9df8d6bb75cb2
johnsan7 <johnsan7@oregonstate.edu>
2015-12-03 18:06:54 -0600

    j

--- serverService.js
+++ serverService.js
@@ -51,7 +51,7 @@ app.get('/reset-table',function(req,res,next){
     "weight INT,"+
     "date DATE,"+
     "lbs BOOLEAN)";
-    mysql.pool.query(createString, function(err){
+    pool.query(createString, function(err){
       context.results = "Table reset";
       res.send("Table Reset");
     })
