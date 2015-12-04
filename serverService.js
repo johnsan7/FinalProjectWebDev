d2c330353ee084ce0703cf21e81c8884f6c0484a
johnsan7 <johnsan7@oregonstate.edu>
2015-12-03 18:04:00 -0600

    h

--- serverService.js
+++ serverService.js
@@ -80,14 +80,16 @@ console.log("Got to after the mysql query");
 });
 
 app.get('/insert',function(req,res,next){
-  var context = {};
-  pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Inserted id " + result.insertId;
-    res.send(context.results);
+console.log("Getting into insert");
+	  var context = {};
+	  pool.query("INSERT INTO workouts (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
+		if(err){
+		  next(err);
+		  return;
+		}
+		console.log("Got past that insert stuff");
+		context.results = "Inserted id " + result.insertId;
+		res.send(context.results);
   });
 });
 
