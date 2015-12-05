7a314d6cf6b870c94aee71b7ad317195b4f9aafc
johnsan7 <johnsan7@oregonstate.edu>
2015-12-05 16:16:32 -0600

    arver

--- public/client.js
+++ public/client.js
@@ -238,6 +238,20 @@ function buttonAssign()
 					{
 						var updateReq = new XMLHttpRequest();
 						
+						//If another form exists, delete it
+							if(document.getElementsByClassName("editTableClass") != null)
+							{			
+								
+								var oldtable = document.getElementsByClassName("editTableClass");
+								for(var thing in oldtable)
+								{
+									oldtable.parentNode.removeChild(oldtable);	
+								}
+								
+
+							}
+						
+						
 						var url = 'http://ec2-52-26-46-121.us-west-2.compute.amazonaws.com:1976/generate-update-form-data' + '?id=' + edBut.value;
 						updateReq.open('GET', url, true);
 						updateReq.addEventListener('load', function()
@@ -251,6 +265,7 @@ function buttonAssign()
 								
 								var editForm = document.createElement('form');
 								editForm.id = edBut.value;
+								editForm.class="editTableClass";
 								
 								var fieldsetEditForm = document.createElement('fieldset');
 								
