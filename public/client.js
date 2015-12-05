3db3acfef1777b8ec08e4fc95260cdd8bae11e41
johnsan7 <johnsan7@oregonstate.edu>
2015-12-05 14:22:53 -0600

    fred

--- public/client.js
+++ public/client.js
@@ -2,18 +2,10 @@
 //This program is a game where there are 100 random zip codes from US locations. You have to guess the temperature within 5 degrees without knowing the name
 //of the city. You get 3 points for a correct answer and lose 1 point for an incorrect. 
 
-//var express = require('express');
-
-//var app = express();
 
 
 
 
-console.log("Client Javascript is running");
-
-//drawTable();
-
-//app.use(express.static('public'));
 
 document.addEventListener('DOMContentLoaded', buttonSet);
 document.addEventListener('DOMContentLoaded', function()
@@ -45,7 +37,7 @@ console.log("gets into drawTable at least");
 	//This removes the current table if there is one
 	if(document.getElementById("dataTable") != null)
 	{			
-		console.log("Table deleting triggered WANT TO BE VSISIBLE TOO MANY PRINT STATEMENTS!!!!!");
+		console.log("Table deleting triggered -------------------------------------------------------------------------------------------------");
 		var eltable = document.getElementById("dataTable");
 		eltable.parentNode.removeChild(eltable);
 
@@ -57,10 +49,10 @@ console.log("gets into drawTable at least");
 	{
 		if(req.status >= 200 && req.status < 400)
 		{
-			console.log("Request was returned");
-			console.log("Respons was: ", req.responseText);
+			//console.log("Request was returned");
+			//console.log("Respons was: ", req.responseText);
 			var response = JSON.parse(req.responseText);  
-			console.log("after parsing response was: ", response);
+			//console.log("after parsing response was: ", response);
 			var newTable = document.createElement('table');
 			newTable.id="dataTable";
 			
@@ -68,7 +60,7 @@ console.log("gets into drawTable at least");
 			var newRow = document.createElement('tr');
 			var newBody = document.createElement("tbody");
 			
-			console.log("Created header stubs");		
+			//console.log("Created header stubs");		
 			
 			var nameTitle = document.createElement('th');
 			nameTitle.textContent = 'Name';
@@ -93,16 +85,16 @@ console.log("gets into drawTable at least");
 			newHead.appendChild(newRow);
 			//The above should build the header. Next we can do the actual data in the body
 			//Now we need to build the body, and to do that we need to do a loop, I think for each will work. 
-			console.log("Appended headers to head");		
+			//console.log("Appended headers to head");		
 			for(var thing in response)
 			{
-				console.log("Appending rows in loop");
+				//console.log("Appending rows in loop");
 				var nextRow = document.createElement('tr');
 				
 				var nameBox = document.createElement('td');
 				nameBox.textContent=response[thing].name;
 				nextRow.appendChild(nameBox);
-				console.log("Doing name: ", response[thing].name);
+				//console.log("Doing name: ", response[thing].name);
 				var repsBox = document.createElement('td');
 				repsBox.textContent=response[thing].reps;
 				nextRow.appendChild(repsBox);
@@ -160,17 +152,16 @@ console.log("gets into drawTable at least");
 				buttonRow.appendChild(hidDeleteID);
 				buttonRow.appendChild(editButton);
 				buttonRow.appendChild(hidUpdateID);
-console.log("hidden for delete is: ", hidDeleteID.value);
+
 				newBody.appendChild(buttonRow);
 				
 			
 				
 			}
-			console.log("Done appending rows");
 			newTable.appendChild(newHead);
 			newTable.appendChild(newBody);
 			document.body.appendChild(newTable);
-			console.log("Table actually got appended, now calling button assigner");
+
 			buttonAssign();
 			
 
@@ -199,7 +190,7 @@ function buttonAssign()
 				{
 					return function()
 					{
-						console.log("delete button id is: ", delBut.value);  //Note to test git. 
+						//console.log("delete button id is: ", delBut.value);  //Note to test git. 
 						var url = 'http://ec2-52-26-46-121.us-west-2.compute.amazonaws.com:1976/delete' + '?id=' + delBut.value;
 						delReq.open('GET', url, true);
 						delReq.addEventListener('load', function()
@@ -207,14 +198,14 @@ function buttonAssign()
 							if(delReq.status >= 200 && delReq.status < 400)
 							{
 								document.body.removeChild;
-								console.log("Great, deleted row");
-								console.log("Row index test, row index is:", delBut.rowIndex);
+								//console.log("Great, deleted row");
+								//console.log("Row index test, row index is:", delBut.rowIndex);
 								//delBut.nextSibling.nextSibling.nextSibling.removeNode;
 								//delBut.nextSibling.nextSibling.removeNode;
 								//delBut.nextSibling.removeNode;
 								//delBut.parentNode.removeChild(delBut);
 								console.log("Drawing table in button Assign --------------------------------------------------------------")
-								drawTable();
+								//drawTable();
 							}
 							else
 							{
