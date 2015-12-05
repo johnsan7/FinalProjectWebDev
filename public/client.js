1ac7b0f57e534858cfc3804dae000382dd3ccb68
johnsan7 <johnsan7@oregonstate.edu>
2015-12-05 15:59:03 -0600

    asdf

--- public/client.js
+++ public/client.js
@@ -247,7 +247,7 @@ function buttonAssign()
 								console.log("getting into response");
 								var editResponse = JSON.parse(updateReq.responseText);
 								
-								console.log("here is the response name", editResponse.name);
+								console.log("here is the response name", updateReq.responseText);
 								
 								var editForm = document.createElement('form');
 								editForm.id = edBut.value;
